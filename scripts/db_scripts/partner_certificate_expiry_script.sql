23ff5ada091a978a22dc833ab8bac9a8d29c130d
trialblazerseee <84778104+trialblazerseee@users.noreply.github.com>
2022-11-30 11:20:55 +0530

    New Dashboard Development for Partner Certificate Expiry

--- scripts/db_scripts/partner_certificate_expiry_script.sql
+++ scripts/db_scripts/partner_certificate_expiry_script.sql
@@ -25,17 +25,29 @@ returns trigger SECURITY DEFINER
 as $$
 begin
     TRUNCATE TABLE PARTNER_CERTIFICATE_EXPIRY;
-    INSERT INTO PARTNER_CERTIFICATE_EXPIRY (SELECT P.CERT_ID, SPLIT_PART(SPLIT_PART(P.CERT_SUBJECT, ',', 1),'=',2) AS COMMON_NAME, P.ORGANIZATION_NAME, P.PARTNER_DOMAIN, P.CERT_NOT_AFTER AS PARTNER_EXPIRY_ON,
+    INSERT INTO PARTNER_CERTIFICATE_EXPIRY (SELECT P.CERT_ID, SPLIT_PART(SPLIT_PART(P.CERT_SUBJECT, ',', 1),'=',2) AS COMMON_NAME,
+                                            P.ORGANIZATION_NAME, P.PARTNER_DOMAIN, P.CERT_NOT_AFTER AS PARTNER_EXPIRY_ON,
                                             CASE WHEN CA.CA2_EXPIRY_ON IS NOT NULL THEN CA.CA1_EXPIRY_ON ELSE NULL END AS INTER_EXPIRY_ON,
                                             CASE WHEN CA.CA2_EXPIRY_ON IS NOT NULL THEN CA.CA2_EXPIRY_ON ELSE CA.CA1_EXPIRY_ON END AS CA_EXPIRY_ON,
                                             CA3.MOSIP_EXPIRY_ON
                                             FROM keymgr.partner_cert_store P
                                             LEFT JOIN (
-                                            SELECT CA1.CERT_ID, CA1.CERT_NOT_AFTER AS CA1_EXPIRY_ON, CA2.CERT_NOT_AFTER AS CA2_EXPIRY_ON FROM  keymgr.ca_cert_store CA1
-                                            LEFT JOIN keymgr.ca_cert_store CA2 ON CA2.CERT_ID = CA1.ISSUER_ID AND CA1.CERT_ID != CA1.ISSUER_ID
-                                            ) CA ON CA.CERT_ID = P.ISSUER_ID
-                                            LEFT JOIN (SELECT CERT_SUBJECT, CERT_ISSUER, MAX(CERT_NOT_AFTER) AS MOSIP_EXPIRY_ON FROM keymgr.ca_cert_store GROUP BY CERT_SUBJECT, CERT_ISSUER) CA3 ON CA3.CERT_SUBJECT = P.CERT_SUBJECT AND CA3.CERT_ISSUER LIKE '%MOSIP-TECH-CENTER (PMS)%');
-                                            RETURN NULL;
+                                                    SELECT CA1.CERT_ID, CA1.CERT_NOT_AFTER AS CA1_EXPIRY_ON, CA2.CERT_NOT_AFTER AS CA2_EXPIRY_ON
+                                                    FROM  keymgr.ca_cert_store CA1
+                                                    LEFT JOIN keymgr.ca_cert_store CA2 ON CA2.CERT_ID = CA1.ISSUER_ID AND CA1.CERT_ID != CA1.ISSUER_ID
+                                                    ) CA ON CA.CERT_ID = P.ISSUER_ID
+                                            LEFT JOIN (
+                                                    SELECT CERT_SUBJECT, CERT_ISSUER, MAX(CERT_NOT_AFTER) AS MOSIP_EXPIRY_ON
+                                                    FROM keymgr.ca_cert_store GROUP BY CERT_SUBJECT, CERT_ISSUER
+                                                    ) CA3 ON CA3.CERT_SUBJECT = P.CERT_SUBJECT AND CA3.CERT_ISSUER LIKE '%MOSIP-TECH-CENTER (PMS)%'
+                                            INNER JOIN (
+                                                    SELECT PARTNER_DOMAIN, SPLIT_PART(SPLIT_PART(CERT_SUBJECT, ',', 1),'=',2) AS COMMON_NAME,
+                                                    ORGANIZATION_NAME, MAX(CR_DTIMES) AS MAX_DT
+                                                    FROM keymgr.partner_cert_store
+                                                    GROUP BY  PARTNER_DOMAIN, SPLIT_PART(SPLIT_PART(CERT_SUBJECT, ',', 1),'=',2), ORGANIZATION_NAME
+                                                    ) EX ON EX.PARTNER_DOMAIN = P.PARTNER_DOMAIN AND EX.COMMON_NAME = SPLIT_PART(SPLIT_PART(P.CERT_SUBJECT, ',', 1),'=',2)
+                                                    AND EX.ORGANIZATION_NAME=P.ORGANIZATION_NAME AND EX.MAX_DT=P.CR_DTIMES);
+    RETURN NULL;
 end $$ language plpgsql;
 create trigger ca_cert_store_trigger after insert or update or delete or truncate on ca_cert_store for each statement execute procedure partner_certificate_expiry_trigger();
 create trigger partner_cert_store_view after insert or update or delete or truncate on partner_cert_store for each statement execute procedure partner_certificate_expiry_trigger();
